564dba06de8b0c9d968acd806115125ae9ead3c1
Marco Ullmann <marco.ullmann@namics.com>
2021-03-12 09:34:00 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Master Thesis (MAS Data Science) of Marco Ullmann in 2021 about recommender systems.',
+    author='Marco Ullmann',
+    license='',
+)
